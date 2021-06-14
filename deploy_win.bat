aaaa7c4079f9aaa76dfa94a9b409d20d4ff25a1e
cangyin <1522645226@qq.com>
2021-06-14 19:12:43 +0800

    update deployment

--- deploy_win.bat
+++ deploy_win.bat
@@ -2,9 +2,16 @@
 rmdir /s /q dist
 
 echo ----------- Building package -----------
-pyinstaller -F main.py ^
-    -n pqv2ray ^
-    --noupx 
+if defined CI (
+    pyinstaller -F main.py ^
+        -n pqv2ray ^
+        --windowed ^
+        --noupx 
+) else (
+    pyinstaller -F main.py ^
+        -n pqv2ray ^
+        --noupx 
+)
 
 echo ----------- Copy additional files -----------
 xcopy /S components\config dist\components\config\
