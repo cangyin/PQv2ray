8e7fb586d1a895dab85a3f20c9cc8da521492ba6
cangyin <1522645226@qq.com>
2021-06-14 19:20:53 +0800

    remove unexpected import

--- components/utils.py
+++ components/utils.py
@@ -10,7 +10,6 @@ from traceback import format_exc
 import subprocess as subp
 import shlex
 from shutil import copy as copy_file
-from typing_extensions import TypeGuard
 
 import psutil
 
@@ -42,7 +41,7 @@ def read_text_file(file :str):
 
 def write_text_file(file :str, text :str, mkdirs :bool=True):
     if mkdirs:
-        os.makedirs(path.dirname(file), exist_ok=True)
+        os.makedirs(path.abspath(path.dirname(file)), exist_ok=True)
     with open(file, 'wt', encoding=file_encoding) as f:
         f.write(text)
 
